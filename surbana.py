c16d7a2c1db78d86cd3d74facad326b936337b8e
banu-07 <58077638+banu-07@users.noreply.github.com>
2020-03-17 10:24:17 +0400

    Add files via upload

---
+++ surbana.py
@@ -0,0 +1,25 @@
+class technology:
+    'class of tech learners and mentors'
+    availablelearner=0;
+    availablementor=0;
+    availablecount=0;
+    time=0;
+    def addstacks(self,interest,expertise):
+        self.interest=interest;
+        self.expertise=expertise;
+        technology.availablecount+=1;
+    def setMentororLearner(self):
+        if self.expertise!=0;
+        technology.availablementor+=1;
+
+        else
+        technology.availablelearner+=1;
+     def setAvailableTime(self):
+       if technology.availablementor==1;
+       time=input(time);
+     def getMentor(addstacks,time)
+           addstacks();
+           setMentororLearner();
+           setAvailableTime();
+           print "availablementor:",self.availablementor
+
